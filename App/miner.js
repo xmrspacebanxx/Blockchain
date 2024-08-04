de6b83b73af07efff4135606c413a8079089c2b0
xmrspacebanxx <xmrspacebanxx@gmail.com>
2024-08-04 16:20:22 -0500

    movil

--- App/miner.js
+++ App/miner.js
@@ -1,50 +1,108 @@
 
-let { isMining, miningTimeout } = require('../config');
-const { MINING_INTERVAL } = require('../config');
+const { Worker } = require('worker_threads');
+let { isMining, miningTimeout, numWorkers } = require('../config');
+const { MINING_INTERVAL, TARGET_TIME } = require('../config');
 const Transaction = require("../Wallet/transactions");
 const Wallet = require('../Wallet/index');
 
-
-class Miner{
-    constructor(blockchain, transactionPool, wallet, p2pServer){
+class Miner {
+    constructor(blockchain, transactionPool, wallet, p2pServer) {
         this.blockchain = blockchain;
         this.transactionPool = transactionPool;
         this.wallet = wallet;
         this.p2pServer = p2pServer;
+        this.controlFlag = { found: false };
+        this.workers = [];
     }
 
-    mine(){
-        if(isMining) {
-            console.log('Mining block...');
+    mine() {
+        if (isMining) {
             const validTransactions = this.transactionPool.validTransactions();
-            //include a reward for the miner
             validTransactions.push(Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet()));
-            //create a block consisting of the valids transactions
-            const block = this.blockchain.addBlock(validTransactions);
-            console.log(`New block added: ${block.toString()}`);
-            //synchronize the chains in the peep to peer server
-            this.p2pServer.syncChains();
-            //clear the transactions pool
-            this.transactionPool.clear();
-            //broadcast to every miner to clear the transactions pool
-            this.p2pServer.broadcastClearTransactions();
-            //interval mining
-            this.intervalMining();
-            return block;    
+            this.mining(validTransactions);
         }
     }
+
+    mining(transactions) {
+        const lastBlock = this.blockchain.getLastBlock();
+        const nonceRange = Math.pow(2, 32) / numWorkers;
+
+        this.workers = this.createWorkers(numWorkers, lastBlock, transactions, nonceRange);
+
+        this.workers.forEach((worker) => {
+            worker.on('message', (message) => {
+                if (message) {
+                    const { block, transactions } = message;
+                    console.log(`New block added by worker: ${block.toString()}`);
+                    this.blockchain.chain.push(block);
+                    this.p2pServer.syncChains();
+                    this.transactionPool.clear(transactions);
+                    this.p2pServer.broadcastClearTransactions();
+                    this.stopAllWorkers();
+                    this.adjustWorkers(processTime);
+                    this.intervalMining();
+                } else {
+                    console.log('Worker stopped mining as block was found by another worker.');
+                }
+            });
+
+            worker.on('error', (error) => {
+                console.error(`Worker error: ${error}`);
+            });
+
+            worker.on('exit', (code) => {
+                if (code !== 0) {
+                    console.error(`Worker stopped with exit code ${code}`);
+                }
+            });
+        });
+    }
+
+    createWorkers(num, lastBlock, transactions, nonceRange) {
+        const workers = [];
+        for (let i = 0; i < num; i++) {
+            const nonceStart = Math.floor(i * nonceRange);
+            const nonceEnd = Math.floor((i + 1) * nonceRange);
+
+            workers.push(new Worker('./minerWorker.js', {
+                workerData: { lastBlock, transactions, nonceStart, nonceEnd, controlFlag: this.controlFlag }
+            }));
+        }
+        return workers;
+    }
+
+    stopAllWorkers() {
+        this.workers.forEach(worker => worker.terminate());
+        this.controlFlag.found = false;
+    }
+
+    adjustWorkers(processTime) {
+        if (processTime > TARGET_TIME && numWorkers > 1) {
+            numWorkers--;
+            console.log(`Decreasing number of workers to ${numWorkers}`);
+        } else if (processTime < TARGET_TIME && numWorkers < 10) {
+            numWorkers++;
+            console.log(`Increasing number of workers to ${numWorkers}`);
+        }
+    }
+
     startMining() {
-        if(!isMining) {
+        if (!isMining) {
             isMining = true;
             this.mine();
             console.log('Mining started');
         }
     }
+
     stopMining() {
-        clearTimeout(miningTimeout);
-        isMining = false;
-        console.log('Mining stopped');
+        if (isMining) {
+            isMining = false;
+            clearTimeout(miningTimeout);
+            this.stopAllWorkers(this.workers);
+            console.log('Mining stopped');
+        }
     }
+
     intervalMining() {
         miningTimeout = setTimeout(() => {
             this.mine();
--- Blockchain/Block.js
+++ Blockchain/Block.js
@@ -64,7 +64,7 @@ class Block {
         let processTime = t2 - t1;
         controlFlag.found = true;
 
-        const block = new this(timestamp, lastHash, hash, data, nonce, difficulty, processTime);
+        const block = new this(timestamp, lastHash, '0'.repeat(difficulty) + hash.substring(difficulty), data, nonce, difficulty, processTime);
         parentPort.postMessage({ block, transactions: data });
     }
 
@@ -74,7 +74,8 @@ class Block {
 
     static blockHash(block) {
         const { timestamp, lastHash, data, nonce, difficulty } = block;
-        return this.hash(timestamp, lastHash, data, nonce, difficulty);
+        const hash = this.hash(timestamp, lastHash, data, nonce, difficulty);
+        return '0'.repeat(difficulty) + hash.substring(difficulty);
     }
     
     static adjustDifficulty(lastBlock, currentTime){
